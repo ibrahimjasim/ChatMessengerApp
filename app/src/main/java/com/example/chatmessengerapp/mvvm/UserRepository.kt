82ad841cb9e03489e93996cc7e776a48423ecf3a
Pinar <pinarbildirici87@gmail.com>
2026-01-05 00:59:07 +0100

    Implement UserRepository to fetch users
    Adds a `UserRepository` class with a `getUser` method. This method fetches a list of users from the "users" collection in Firestore. It listens for real-time updates and excludes the currently logged-in user from the list. The user list is exposed as `LiveData`.

--- app/src/main/java/com/example/chatmessengerapp/mvvm/UserRepository.kt
+++ app/src/main/java/com/example/chatmessengerapp/mvvm/UserRepository.kt
@@ -1,4 +1,52 @@
 package com.example.chatmessengerapp.mvvm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.chatmessengerapp.Utils
+import com.example.chatmessengerapp.module.Messages
+import com.example.chatmessengerapp.module.RecentChats
+import com.example.chatmessengerapp.module.Users
+import com.example.chatmessengerapp.notifications.entity.Token
+import com.google.firebase.firestore.FirebaseFirestore
+
 class UserRepository {
+
+    private  var firestore = FirebaseFirestore.getInstance()
+
+
+    fun getUser(): LiveData<List<Users>> {
+
+        val users = MutableLiveData<List<Users>>()
+
+
+        firestore.collection("users").addSnapshotListener { snapshot, exception->
+
+
+            if (exception != null) {
+
+                return@addSnapshotListener
+            }
+
+
+            val usersList =  mutableListOf<Users>()
+            snapshot?.documents?.forEach { document ->
+
+                val user = document.toObject(Users::class.java)
+
+            if (users!!.userid !=Utils.getUiLoggedIn()){
+                user.let {
+                    userList.add(it)
+                }
+
+            }
+
+                users.value = userList
+            }
+
+        }
+
+        return users
+    }
+
+
 }
