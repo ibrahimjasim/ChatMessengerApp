2efadc93a3989d068d8af5476baee061806db1fa
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2026-01-05 21:59:27 +0100

    Convert Utils class to a singleton object

--- app/src/main/java/com/example/chatmessengerapp/Utils.kt
+++ app/src/main/java/com/example/chatmessengerapp/Utils.kt
@@ -2,15 +2,11 @@ package com.example.chatmessengerapp
 
 import com.google.firebase.auth.FirebaseAuth
 
-class Utils {
+object Utils {
 
-    companion object {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
-        private val auth = FirebaseAuth.getInstance()
-
-        fun getUiLogged(): String {
-            // If auth.currentUser is not null, return its uid. Otherwise, return an empty string.
-            return auth.currentUser?.uid ?: ""
-        }
+    fun getUidLoggedIn(): String {
+        return auth.currentUser?.uid ?: ""
     }
 }
