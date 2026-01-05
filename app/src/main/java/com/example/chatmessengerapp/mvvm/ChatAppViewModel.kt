78412a785e09fda87d098bbad8b417c36229e2d0
Pinar <pinarbildirici87@gmail.com>
2026-01-05 04:11:06 +0100

    Fetch and display current user data
    - Implement `getCurrentUser` function in `ChatAppViewModel` to retrieve the logged-in user's data from Firestore.
    - Use `viewModelScope` for the coroutine to handle the asynchronous operation.
    - Update `name` and `imageUrl` LiveData with the fetched user information.
    - Store the username in `SharedPrefs`.
    - Change `ChatAppViewModel` to inherit from `ViewModel`.

--- app/src/main/java/com/example/chatmessengerapp/mvvm/ChatAppViewModel.kt
+++ app/src/main/java/com/example/chatmessengerapp/mvvm/ChatAppViewModel.kt
@@ -2,13 +2,22 @@ package com.example.chatmessengerapp.mvvm
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chatmessengerapp.MyApplication
+import com.example.chatmessengerapp.SharedPrefs
+import com.example.chatmessengerapp.Utils
 import com.example.chatmessengerapp.module.Users
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ChatAppViewModel {
+class ChatAppViewModel : ViewModel(){
 
     val name = MutableLiveData<String>()
     val imageUrl = MutableLiveData<String>()
     val message = MutableLiveData<String>()
+    private val firestore = FirebaseFirestore.getInstance()
 
     val usersRepo = UserRepository()
 
@@ -21,6 +30,27 @@ class ChatAppViewModel {
     }
 
 
+    fun getCurrentUser() = viewModelScope.launch(Dispatchers.IO) {
+
+        val context = MyApplication.instance.applicationContext
+
+        firestore.collection("Users").document(Utils.getUiLogged())
+            .addSnapshotListener { value, error ->
 
 
-}
+                if (value!!.exists() && value.data != null) {
+
+                    val users = value.toObject(Users::class.java)
+
+                    name.value = users?.username!!
+                    imageUrl.value = users?.imageUrl!!
+
+                    val mysharedPrefs = SharedPrefs(context)
+                    mysharedPrefs.setValue("username", users.username)
+
+
+                }
+
+            }
+
+    }
