df715290b132e7f03024abb4aaab7adc2279eca6
Pinar <pinarbildirici87@gmail.com>
2026-01-05 01:08:43 +0100

    Initialize variables in HomeFragment

--- app/src/main/java/com/example/chatmessengerapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/chatmessengerapp/fragments/HomeFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chatmessengerapp.Adapter.UserAdapter
 import com.example.chatmessengerapp.R
+import com.example.chatmessengerapp.databinding.FragmentHomeBinding
+import com.example.chatmessengerapp.mvvm.ChatAppViewModel
 
 class HomeFragment : Fragment() {
+
+    lateinit var ry : RecyclerView
+    lateinit var useradapter : UserAdapter
+    lateinit var userViewModel : ChatAppViewModel
+    lateinit var binding : FragmentHomeBinding
+
 
 
     override fun onCreateView(
