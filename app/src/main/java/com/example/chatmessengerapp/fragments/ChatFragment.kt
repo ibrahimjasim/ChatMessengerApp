93effda8745260f623effa33b383a36aa1c082b8
mazlum <mazlum@hotmail.com>
2026-01-05 18:26:42 +0100

    Implement ChatFragment logic and data binding
    - Initialize data binding and ViewModel in `ChatFragment`.
    - Setup toolbar components including user image, status, and username using navigation arguments.
    - Implement back button functionality to navigate to HomeFragment.
    - Integrate Glide for user profile image loading.

--- app/src/main/java/com/example/chatmessengerapp/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/chatmessengerapp/fragments/ChatFragment.kt
@@ -5,9 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.chatmessengerapp.R
+import com.example.chatmessengerapp.databinding.FragmentChatBinding
+import com.example.chatmessengerapp.mvvm.ChatAppViewModel
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ChatFragment : Fragment() {
+
+     private lateinit var args: ChatFragmentArgs
+     private lateinit var  chatbinding: FragmentChatBinding
+     private lateinit var chatAppViewModel: ChatAppViewModel
+     private lateinit var chattoolbar: Toolbar
+
+     private lateinit var circleImageView: circleImageView
+     private lateinit var tvUserName: TextView
+     private lateinit var tvStatus: TextView
+     private lateinit var backbtn : ImageView
+
+
+
 
 
     override fun onCreateView(
@@ -15,8 +35,35 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        chatbinding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat, container, false)
+        return chatbinding.root
     }
 
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+      args = ChatFragmentArgs.fromBundle(requireArguments())
 
+      chatAppViewModel = ViewModelProvider(this).get(ChatAppViewModel::class.java)
+
+      chattoolbar = view.findViewById(R.id.toolBarChat)
+      circleImageView = chattoolbar.findViewById(R.id.chatImageViewUser)
+      tvStatus = chattoolbar.findViewById(R.id.chatUserStatus)
+      backbtn = chattoolbar.findViewById (R.id.chatBackBtn)
+
+      backbtn.setOnClickListener {
+          view.findNavController().navigate(R.id.action_chatFragment_to_homeFragment)
+
+      }
+      Glide.with(requireContext()).load(args.users.imageUrl).into(circleImageView)
+      tvStatus.setText(args.users.status)
+      tvUserName.setText(args.users.username)
+
+
+
+      chatbinding.viewModel = chatAppViewModel
+      chatbinding.lifecycleOwner = viewLifecycleOwner
+
+      chattoolbar = chatbinding.toolbarChat
+
+  }
 }
