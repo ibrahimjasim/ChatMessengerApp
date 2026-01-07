f97950c9c38f4768ffa0db815cc5bf9fb70721c0
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2026-01-07 11:10:56 +0100

    Enhance and restructure  Utils class Converted the `Utils` object into a class with a companion object to better manage utility functions and constants.

--- app/src/main/java/com/example/chatmessengerapp/Utils.kt
+++ app/src/main/java/com/example/chatmessengerapp/Utils.kt
@@ -1,20 +1,50 @@
 package com.example.chatmessengerapp
 
+import android.annotation.SuppressLint
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.Date
 
-object Utils {
+class Utils {
 
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        val context = MyApplication.instance.applicationContext
+        @SuppressLint("StaticFieldLeak")
+        val firestore = FirebaseFirestore.getInstance()
+
+
+
+
 
         private val auth = FirebaseAuth.getInstance()
+        private var userid: String = ""
+        const val REQUEST_IMAGE_CAPTURE = 1
+        const val REQUEST_IMAGE_PICK = 2
+        const val MESSAGE_RIGHT = 1
+        const val MESSAGE_LEFT = 2
+        const val CHANNEL_ID = "com.example.chatmessenger"
 
-        fun getUiLogged(): String {
-            // If auth.currentUser is not null, return its uid. Otherwise, return an empty string.
-            return auth.currentUser?.uid ?: ""
+
+        fun getUidLoggedIn(): String {
+
+            if (auth.currentUser!=null){
+
+
+                userid = auth.currentUser!!.uid
+
+
+
+            }
+
+
+            return userid
+
+
+
         }
-
 
         fun getTime(): String {
 
@@ -26,11 +56,11 @@ object Utils {
 
             return stringdate
 
+        }
+
+
+
 
 
     }
-
-
-
-
 }
