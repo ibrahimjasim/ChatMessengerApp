4bd8e9ddfd121d77850e4e244278c2f1e3cd2871
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2026-01-07 23:53:52 +0100

    Removes an unnecessary empty body from the secondary constructor in the `Users` data class.

--- app/src/main/java/com/example/chatmessengerapp/module/Users.kt
+++ app/src/main/java/com/example/chatmessengerapp/module/Users.kt
@@ -17,8 +17,7 @@ data class Users (
          parcel.readString(),
          parcel.readString(),
          parcel.readString()
-     ) {
-     }
+     )
 
      override fun writeToParcel(parcel: Parcel, flags: Int) {
          parcel.writeString(userid)
