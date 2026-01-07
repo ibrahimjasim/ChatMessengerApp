efe09fa7eecfa76f844687d4957dbc94202985de
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2026-01-07 23:51:23 +0100

    updates dependencies and reorganizes the `app/build.gradle.kts` file for better clarity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("kotlin-parcelize")                 // for @Parcelize
     id("com.google.gms.google-services")
     id("androidx.navigation.safeargs.kotlin")
 }
@@ -29,15 +30,17 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 
-    buildFeatures{
+    buildFeatures {
         dataBinding = true
         viewBinding = true
     }
@@ -45,31 +48,33 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Core
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.8.2")
+
+    // Navigation (SafeArgs works with these)
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.activity:activity:1.8.2")
+
+    // Firebase (BoM manages versions)
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    // UI
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    // Glide (versions MUST match)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
-
-    // Add the dependency for the Firebase Authentication library
-    implementation("com.google.firebase:firebase-auth-ktx")
-
-    // Also add the dependency for the Cloud Firestore library
-    implementation("com.google.firebase:firebase-firestore-ktx")
-
-    implementation("com.google.firebase:firebase-analytics")
-
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
-
-    //Glide
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    kapt("com.github.bumptech.glide:compiler:4.12.0")
 }
