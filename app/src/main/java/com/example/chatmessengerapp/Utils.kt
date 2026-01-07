62d320a19d92e003eaa1aee7ac2eac9e02c7c267
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2026-01-07 11:10:56 +0100

    Enhance and restructure  Utils class Converted the `Utils` object into a class with a companion object to better manage utility functions and constants.

--- app/src/main/java/com/example/chatmessengerapp/Utils.kt
+++ app/src/main/java/com/example/chatmessengerapp/Utils.kt
@@ -1,22 +1,66 @@
 package com.example.chatmessengerapp
 
+import android.annotation.SuppressLint
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.Locale
 
-object Utils {
+class Utils {
 
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
-    fun getUiLogged(): String {
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        val context = MyApplication.instance.applicationContext
+        @SuppressLint("StaticFieldLeak")
+        val firestore = FirebaseFirestore.getInstance()
 
-        return auth.currentUser?.uid ?: ""
-    }
 
-    fun getTime(): String {
-        val formatter = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
-        val date = Date(System.currentTimeMillis())
-        return formatter.format(date)
+
+
+
+        private val auth = FirebaseAuth.getInstance()
+        private var userid: String = ""
+        const val REQUEST_IMAGE_CAPTURE = 1
+        const val REQUEST_IMAGE_PICK = 2
+        const val MESSAGE_RIGHT = 1
+        const val MESSAGE_LEFT = 2
+        const val CHANNEL_ID = "com.example.chatmessenger"
+
+
+        fun getUidLoggedIn(): String {
+
+            if (auth.currentUser!=null){
+
+
+                userid = auth.currentUser!!.uid
+
+
+
+            }
+
+
+            return userid
+
+
+
+        }
+
+        fun getTime(): String {
+
+
+            val formatter = SimpleDateFormat("HH:mm:ss")
+            val date: Date = Date(System.currentTimeMillis())
+            val stringdate = formatter.format(date)
+
+
+            return stringdate
+
+        }
+
+
+
+
+
     }
 }
