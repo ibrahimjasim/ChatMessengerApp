4beb8839809115428a04c5790bd1aef07cdd1111
mazlum <mazlum@hotmail.com>
2026-01-10 00:17:59 +0100

    Update time to Timestamp and use @Parcelize
    Updated the `RecentChats` data class in both the `mvvm` and `module` packages.
    
    - Changed the `time` property type from `String` to `Firebase.Timestamp` to handle time more effectively.
    - Implemented `@Parcelize` to simplify the `Parcelable` implementation, removing boilerplate code.

--- app/src/main/java/com/example/chatmessengerapp/module/RecentChats.kt
+++ app/src/main/java/com/example/chatmessengerapp/module/RecentChats.kt
@@ -1,54 +1,17 @@
 package com.example.chatmessengerapp.module
 
-import android.os.Parcel
 import android.os.Parcelable
+import com.google.firebase.Timestamp
+import kotlinx.parcelize.Parcelize
 
-data class RecentChats(val friendid : String? ="",
-                       val friendsimage: String? = "",
-                       val time : String? = "",
-                       val name: String? ="",
-                       val sender: String? = "",
-                       val message : String? = "",
-                       val person: String? = "",
-                       val status: String? ="",
-
-                       ) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(friendid)
-        parcel.writeString(friendsimage)
-        parcel.writeString(time)
-        parcel.writeString(name)
-        parcel.writeString(sender)
-        parcel.writeString(message)
-        parcel.writeString(person)
-        parcel.writeString(status)
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<RecentChats> {
-        override fun createFromParcel(parcel: Parcel): RecentChats {
-            return RecentChats(parcel)
-        }
-
-        override fun newArray(size: Int): Array<RecentChats?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-
-}
+@Parcelize
+data class RecentChats(
+    val friendid: String? = "",
+    val friendsimage: String? = "",
+    val time: Timestamp? = null,
+    val name: String? = "",
+    val sender: String? = "",
+    val message: String? = "",
+    val person: String? = "",
+    val status: String? = ""
+) : Parcelable
